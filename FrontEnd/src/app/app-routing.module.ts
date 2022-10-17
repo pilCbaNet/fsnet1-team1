87a8765001c39f181347a576d890ee7eeec6f90b
Emiliano Andres <emilianocraviotto@gmail.com>
2022-10-17 11:42:09 -0300

    template componente de transacciones

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -5,9 +5,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AuthGuard } from './guards/auth.guard';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 const routes: Routes = [
-  { path: '', component: LandingpageComponent, canActivate: [AuthGuard] },
+  {
+    path: '', component: LandingpageComponent, canActivate: [AuthGuard],
+    children: [
+      { path: 'transactions', component: TransactionsComponent }
+    ]
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'footer', component: FooterComponent },
@@ -18,4 +24,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
